ad1041dfc481cc01a381bde88c3827aed69dbfc3
Shivdhwaj Pandey <shivdhwaj@gmail.com>
2016-09-21 14:10:40 +0530

    Update convertImage2Base64.py

--- convertImage2Base64.py
+++ convertImage2Base64.py
@@ -1,8 +1,8 @@
 import base64
 import requests
-
-response = requests.get("http://images.indianexpress.com/2016/03/google-doodle-holi_7591.jpg")
-uri = ("data:" + 
+url = "http://images.indianexpress.com/2016/03/google-doodle-holi_7591.jpg"
+response = requests.get(url)
+convertedData = ("data:" + 
                response.headers['Content-Type'] + ";" +
                       "base64," + str(base64.b64encode(response.content).decode("utf-8")))
-print uri
+print convertedData
