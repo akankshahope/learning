74393a115fcac2d8eab3626f4e9d0c40579289cc
Shivdhwaj Pandey <shivdhwaj@browntape.com>
2017-06-14 16:07:51 +0530

    Restructured

--- convertImage2Base64.py
+++
@@ -1,8 +0,0 @@
-import base64
-import requests
-url = "http://images.freeimages.com/images/previews/118/bicycle-sign-3-1442216.jpg"
-response = requests.get(url)
-convertedData = ("data:" + 
-               response.headers['Content-Type'] + ";" +
-                      "base64," + str(base64.b64encode(response.content).decode("utf-8")))
-print convertedData
