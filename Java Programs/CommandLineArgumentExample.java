bdb99fd4a01a11739de65536d8489d7cdd1868a1
Shivdhwaj Pandey <shivdhwaj@browntape.com>
2017-06-14 16:04:37 +0530

    Removed old files

--- Java Programs/CommandLineArgumentExample.java
+++
@@ -1,7 +0,0 @@
-public class CommandLineArgumentExample{
-	public static void main(String[] args){
-		System.out.print("Hi ");
-		System.out.print(args[0]);
-		System.out.println(", You are the first argument from commandline :)");
-	}
-}
